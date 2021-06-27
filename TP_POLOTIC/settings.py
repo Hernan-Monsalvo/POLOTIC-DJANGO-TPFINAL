4fc2ed65d974ea9bb6e8281eebb3359ce941ca4b
hernan <monsalvo.h@hotmail.com>
2021-06-27 19:02:48 -0300

    dos apps usando misma bbdd

--- TP_POLOTIC/settings.py
+++ TP_POLOTIC/settings.py
@@ -77,22 +77,22 @@ WSGI_APPLICATION = 'TP_POLOTIC.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'db',
-    'USER': 'root',
-    'PASSWORD': 'root', 
-    'HOST': 'db'
-    }
-}
-
-#DATABASES = {
-#     'default' : {
-#         'ENGINE' : 'django.db.backends.sqlite3',
-#         'NAME' : BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.mysql',
+#     'NAME': 'db',
+#     'USER': 'root',
+#     'PASSWORD': 'root', 
+#     'HOST': 'db'
 #     }
-#}
+# }
+
+DATABASES = {
+     'default' : {
+         'ENGINE' : 'django.db.backends.sqlite3',
+         'NAME' : BASE_DIR / 'sqlite/db.sqlite3',
+     }
+}
 
 
 # Password validation
